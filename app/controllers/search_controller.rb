0e664e31fb75698606e48df35b0250e0d53d7eca
Zhenyi You <zyi260798@berkeley.edu>
2021-12-01 09:32:59 +0000

    update route, schema, controller and view, zyi

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,10 +6,26 @@ class SearchController < ApplicationController
     def search
         address = params[:address]
         service = Google::Apis::CivicinfoV2::CivicInfoService.new
-        service.key = Rails.application.credentials[:GOOGLE_API_KEY]
+        service.key = Rails.application.credentials.dig(:GOOGLE_API_KEY)
+#         begin
         result = service.representative_info_by_address(address: address)
         @representatives = Representative.civic_api_to_representative_params(result)
-
+#         rescue StandardError
+#             redirect_to representatives_path
+#             flash[:notice] = 'Invalid address'
+#             return
+#         end
         render 'representatives/search'
+    end
+  
+    def show
+        @name = if params[:format].nil?
+                    params[:name]
+                else
+                    params[:name] + '.' + params[:format]
+                end
+
+        @representatives = Representative.where(name: @name).limit(1)
+        render 'representatives/show'
     end
 end
--- app/models/representative.rb
+++ app/models/representative.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+ # frozen_string_literal: true
 
 class Representative < ApplicationRecord
     has_many :news_items, dependent: :delete_all
@@ -9,6 +9,7 @@ class Representative < ApplicationRecord
         rep_info.officials.each_with_index do |official, index|
             ocdid_temp = ''
             title_temp = ''
+            city, street, state, zip = ''
 
             rep_info.offices.each do |office|
                 if office.official_indices.include? index
@@ -16,12 +17,18 @@ class Representative < ApplicationRecord
                     ocdid_temp = office.division_id
                 end
             end
+          
+            official.address&.each do |location|
+                city = location.city, street = location.line1, state = location.state, zip = location.zip
+            end
+          
+            party = official.party unless official.party.nil?
+            photo = official.photo_url unless official.photo_url.nil?            
 
             rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-                title: title_temp })
+                title: title_temp, city: city, state: state, street: street, zip: zip, party: party, photo: photo})
             reps.push(rep)
         end
-
         reps
     end
 end
